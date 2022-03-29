89068774b6381da4eb0218b46cdc9c61d6424868
tunlaton11 <tunlaton11@gmail.com>
2022-03-30 00:28:30 +0700

    adjust

--- src/containers/Course/CoursePage.js
+++ src/containers/Course/CoursePage.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-<<<<<<< HEAD
-=======
 import { useAuth } from '../../contexts/AuthContext';
->>>>>>> 6bb1d138ed4d2de64da85e03eac746a13f125722
 
 const CoursePage = (props) => {
   const auth = useAuth();
@@ -17,39 +14,32 @@ const CoursePage = (props) => {
           <img src={coverImageUrl} className="h-48 w-48 object-cover" alt="" />
         </div>
         <div>
-          <p>
-            {name} (id: {id})
-          </p>
-          <p>Instructor: {instructor}</p>
-          <p>Description: {description}</p>
-          <p>Price: {price} baht</p>
         </div>
       </div>
       <div>
-<<<<<<< HEAD
         <p>
           {name} (id: {id})
         </p>
         <p>Instructor: {instructor}</p>
         <p>Description: {description}</p>
         <p>Price: {price} baht</p>
-        <Link to={`/enroll`}>
-          <button className="rounded-full py-1 px-5 bg-orange-500 hover:bg-orange-600 text-white">
-            Enroll
-          </button>
-        </Link>
-=======
-        {!!auth.user.admin && (
+
+        {!!auth.user.admin ? (
           <Link to={`/course/${id}/createlesson`}>
             <button className="rounded-full py-1 px-5 bg-blue-500 hover:bg-blue-600 text-white">
               Create New Lesson
             </button>
           </Link>
-        )}
->>>>>>> 6bb1d138ed4d2de64da85e03eac746a13f125722
+        ) :
+          <Link to={`/enroll`}>
+            <button className="rounded-full py-1 px-5 bg-blue-500 hover:bg-blue-600 text-white">
+              Enroll
+            </button>
+          </Link>
+        }
       </div>
+
     </>
   );
 };
-
 export default CoursePage;
