ffea04b304d1ef533ed61320e92ddbfdb75b5983
Thanapat Teerarattanyu <37036113+TJTHANAPAT@users.noreply.github.com>
2022-04-12 00:12:51 +0700

    Add Validation + UI

--- src/containers/Enroll/index.js
+++ src/containers/Enroll/index.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
 import { enroll } from '../../functions/enroll';
 import { useAuth } from '../../contexts/AuthContext';
-import { Link } from 'react-router-dom';
+import { Navigate, Link } from 'react-router-dom';
+
+import DatePicker from '@mui/lab/DatePicker';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
+import LoadingPage from '../../components/LoadingPage';
 
 const Enroll = (props) => {
   const auth = useAuth();
@@ -14,14 +30,56 @@ const Enroll = (props) => {
   const paymentDataDefault = {
     cardNumber: '',
     cardHolderName: '',
-    cardCVC: '',
-    cardExpirationMonth: '',
-    cardExpirationYear: '',
+    cardSecurityCode: '',
   };
   const [paymentData, setPaymentData] = useState(paymentDataDefault);
+  const [cardExpiryDate, setCardExpiryDate] = useState(null);
 
-  const handleChangePaymentDataInput = (event) => {
-    setPaymentData({ ...paymentData, [event.target.id]: event.target.value });
+  const [cardNumberInvalid, setCardNumberInvalid] = useState(false);
+  const handleChangeCardNumber = (event) => {
+    setPaymentData({ ...paymentData, cardNumber: event.target.value });
+    if (/^[0-9]{16}$/.test(event.target.value)) {
+      setCardNumberInvalid(false);
+    } else {
+      setCardNumberInvalid(true);
+    }
+  };
+
+  const [cardHolderNameInvalid, setCardHolderNameInvalid] = useState(false);
+  const handleChangeCardHolderName = (event) => {
+    setPaymentData({ ...paymentData, cardHolderName: event.target.value });
+    if (/^[a-zA-Z ]+$/.test(event.target.value)) {
+      setCardHolderNameInvalid(false);
+    } else {
+      setCardHolderNameInvalid(true);
+    }
+  };
+
+  const [cardSecurityCodeInvalid, setCardSecurityCodeInvalid] = useState(false);
+  const handleChangeCardSecurityCode = (event) => {
+    setPaymentData({ ...paymentData, cardSecurityCode: event.target.value });
+    if (/^[0-9]{3}$/.test(event.target.value)) {
+      setCardSecurityCodeInvalid(false);
+    } else {
+      setCardSecurityCodeInvalid(true);
+    }
+  };
+
+  const [cardExpiryDateInvalid, setCardExpiryDateInvalid] = useState(false);
+  const handleExpiryDateError = (err) => {
+    if (!err) {
+      setCardExpiryDateInvalid(false);
+    } else {
+      setCardExpiryDateInvalid(true);
+    }
+  };
+  const handleExpiryDateAccept = (date) => {
+    if (date) {
+      setCardExpiryDateInvalid(false);
+      console.log('accept');
+    } else {
+      setCardExpiryDateInvalid(true);
+    }
   };
 
   const handleSubmitEnroll = async (event) => {
@@ -47,105 +105,208 @@ const Enroll = (props) => {
     }
   };
 
+  const [openDialog, setOpenDialog] = React.useState(false);
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+  };
+
   if (loading) {
-    return <p>Loading...</p>;
+    return <LoadingPage />;
   } else if (success) {
     return (
-      <div>
-        <p>Enrolled successfully.</p>
-        <Link to={`/course/${course.id}`}>Back to course page.</Link>
+      <div className="bg-orange-400 min-h-screen flex flex-col items-center justify-center p-5">
+        <h1 className="text-4xl font-semibold text-white mb-8">Courseiku</h1>
+        <div
+          className="bg-white px-5 py-8 rounded-xl shadow-xl"
+          style={{ minWidth: '300px' }}
+        >
+          <h2 className="text-lg font-medium">ลงทะเบียนเรียนสำเร็จ</h2>
+          <p className="mb-5">คุณได้ทำการลงทะเบียนเรียนคอร์ส {course.name}</p>
+          <Link to={`/course/${course.id}`}>
+            <Button
+              variant="contained"
+              color="secondary"
+              disableElevation
+              fullWidth
+            >
+              ไปยังหน้าคอร์สเรียน
+            </Button>
+          </Link>
+        </div>
+        <p className="text-white mt-16">Courseiku © 2022</p>
       </div>
     );
-  } else if (!!error) {
-    return <p>{error.message}</p>;
   } else if (auth.user.admin) {
     return <p>Admin cannot enroll to the course.</p>;
   } else if (enrolled) {
-    return (
-      <div>
-        <p>You've already enrolled to this course.</p>
-        <Link to={`/course/${course.id}`}>Back to course page.</Link>
-      </div>
-    );
+    return <Navigate to={`/course/${course.id}`} replace />;
   } else {
     return (
-      <div className="bg-orange-400 min-h-screen flex items-center justify-center">
-        <div>
-          <h1 className="text-5xl font-bold text-white mb-5">Enroll</h1>
-          <h3 className="text-2xl font-bold text-white mb-5">
-            You're enrolling course{' '}
-            <span className="text-red-600">{course.name}</span>
-          </h3>
-          <form onSubmit={handleSubmitEnroll}>
-            <div>
-              <label htmlFor="cardNumber">Card Number</label>
-              <input
-                type="text"
-                className="block p-2 rounded border w-full"
-                id="cardNumber"
-                placeholder="Card Number"
-                value={paymentData.cardNumber}
-                onChange={handleChangePaymentDataInput}
-                required
-              />
+      <>
+        <Navbar />
+        <div className="max-w-screen-lg mx-auto my-5 px-5">
+          <h1 className="font-medium text-3xl text-orange-400 mt-16">
+            ลงทะเบียนเรียนคอร์ส {course.name}
+          </h1>
+          <div className="mt-10 flex flex-col md:flex-row">
+            <div className="md:w-1/3 md:pr-8 mb-10">
+              <h2 className="font-medium text-2xl text-orange-400">
+                รายละเอียดคอร์ส
+              </h2>
+              <p>ชื่อคอร์ส : {course.name}</p>
+              <p>ผู้สอน : {course.instructor}</p>
+              <p className="mt-4">
+                ราคา{' '}
+                <span className="text-xl">
+                  {Number(course.price).toLocaleString('en', {
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2,
+                  })}
+                </span>{' '}
+                บาท
+              </p>
             </div>
-            <div>
-              <label htmlFor="cardHolderName">Cardholder Name</label>
-              <input
-                type="text"
-                className="block p-2 rounded border w-full"
-                id="cardHolderName"
-                placeholder="Card Number"
-                value={paymentData.cardHolderName}
-                onChange={handleChangePaymentDataInput}
-                required
-              />
+            <div className="md:w-2/3">
+              <h2 className="font-medium text-2xl text-orange-400">ชำระเงิน</h2>
+              <p>ชำระเงินผ่านบัตรเครดิต/เดบิต</p>
+              <form onSubmit={handleSubmitEnroll}>
+                <div className="mt-3 space-y-3">
+                  <TextField
+                    id="cardNumber"
+                    type="tel"
+                    label="หมายเลขบัตรเครดิต/เดบิต"
+                    value={paymentData.cardNumber}
+                    onChange={handleChangeCardNumber}
+                    error={cardNumberInvalid}
+                    helperText={
+                      cardNumberInvalid
+                        ? 'กรอกหมายเลขบัตรเครดิต/เดบิต 16 หลัก'
+                        : ''
+                    }
+                    inputProps={{
+                      inputMode: 'numeric',
+                      pattern: '[0-9]{16}',
+                      autoComplete: 'cc-number',
+                      maxLength: 16,
+                      placeholder: 'หมายเลขบัตรเครดิต/เดบิต',
+                    }}
+                    variant="outlined"
+                    required
+                    fullWidth
+                    sx={{ backgroundColor: 'rgba(0, 0, 0, 0.02)' }}
+                  />
+                  <TextField
+                    id="cardHolderName"
+                    type="text"
+                    label="ชื่อบนบัตร (ภาษาอังกฤษ)"
+                    value={paymentData.cardHolderName}
+                    onChange={handleChangeCardHolderName}
+                    error={cardHolderNameInvalid}
+                    helperText={
+                      cardHolderNameInvalid ? 'กรอกด้วยภาษาอังกฤษ' : ''
+                    }
+                    inputProps={{
+                      pattern: '[a-zA-Z ]+',
+                      placeholder: 'ชื่อบนบัตร (ภาษาอังกฤษ)',
+                    }}
+                    variant="outlined"
+                    required
+                    fullWidth
+                  />
+                  <div className="flex">
+                    <div className="w-1/2 mr-2">
+                      <LocalizationProvider dateAdapter={AdapterDateFns}>
+                        <DatePicker
+                          inputFormat="MM/yyyy"
+                          views={['year', 'month']}
+                          label="วันหมดอายุ ดด/ปปปป"
+                          minDate={new Date()}
+                          value={cardExpiryDate}
+                          onChange={setCardExpiryDate}
+                          onError={handleExpiryDateError}
+                          onAccept={handleExpiryDateAccept}
+                          renderInput={(params) => (
+                            <TextField
+                              {...params}
+                              variant="outlined"
+                              fullWidth
+                              required
+                            />
+                          )}
+                        />
+                      </LocalizationProvider>
+                    </div>
+                    <div className="w-1/2">
+                      <TextField
+                        id="cardSecurityCode"
+                        type="tel"
+                        label="เลข CCV/CVV"
+                        value={paymentData.cardSecurityCode}
+                        onChange={handleChangeCardSecurityCode}
+                        error={cardSecurityCodeInvalid}
+                        helperText={
+                          cardSecurityCodeInvalid
+                            ? 'กรอกเลข CCV/CVV 3 หลัก'
+                            : ''
+                        }
+                        variant="outlined"
+                        inputProps={{
+                          inputMode: 'numeric',
+                          pattern: '[0-9]{3}',
+                          maxLength: 3,
+                          placeholder: 'เลข CCV/CVV',
+                        }}
+                        required
+                        fullWidth
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <Button
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  disableElevation
+                  fullWidth
+                  sx={{ marginTop: '1rem' }}
+                  disabled={
+                    !paymentData.cardNumber ||
+                    !paymentData.cardHolderName ||
+                    !cardExpiryDate ||
+                    !paymentData.cardSecurityCode ||
+                    cardNumberInvalid ||
+                    cardHolderNameInvalid ||
+                    cardExpiryDateInvalid ||
+                    cardSecurityCodeInvalid
+                  }
+                >
+                  ลงทะเบียนเรียน
+                </Button>
+              </form>
             </div>
-            <div>
-              <label htmlFor="cardExpirationMonth">Expiration Month</label>
-              <input
-                type="text"
-                className="block p-2 rounded border w-full"
-                id="cardExpirationMonth"
-                placeholder="Expiration Month"
-                value={paymentData.cardExpirationMonth}
-                onChange={handleChangePaymentDataInput}
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor="cardExpirationYear">Expiration Year</label>
-              <input
-                type="text"
-                className="block p-2 rounded border w-full"
-                id="cardExpirationYear"
-                placeholder="Expiration Year"
-                value={paymentData.cardExpirationYear}
-                onChange={handleChangePaymentDataInput}
-                required
-              />
-            </div>
-            <div>
-              <label htmlFor="cardCVC">CVC</label>
-              <input
-                type="text"
-                className="block p-2 rounded border w-full"
-                id="cardCVC"
-                placeholder="CVC"
-                value={paymentData.cardCVC}
-                onChange={handleChangePaymentDataInput}
-                required
-              />
-            </div>
-            <button
-              type="submit"
-              className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full w-full mt-5"
-            >
-              Submit
-            </button>
-          </form>
+          </div>
         </div>
-      </div>
+        <Footer />
+        <Dialog
+          open={openDialog}
+          onClose={handleCloseDialog}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">เกิดข้อผิดพลาด</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              {!!error && error.message}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCloseDialog} autoFocus>
+              ปิด
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </>
     );
   }
 };
